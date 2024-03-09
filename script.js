23b04df2c3b79a5b4e605878574b7fd6d7298759
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-03-10 01:42:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let price=document.querySelectorAll('.price')
+	let sum=0;
+	for(let i=0;i<price.length;i++)
+		{
+			sum=price[i]+sum;
+		}
+	return sum;
 };
 
 getSumBtn.addEventListener("click", getSum);
